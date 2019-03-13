5fabf68b933ddf5ede9233828877bb48242b4ed1
williamyeny@gmail.com <williamyeny@gmail.com>
2019-03-13 04:29:54 -0400

    seam-finding dp algo written

--- script.js
+++ script.js
@@ -26,10 +26,7 @@ document.getElementById("file-select").addEventListener("change", function(e) {
       var pixels = renderImage.data
       // edge matrix
       var eMatrix = []
-
-      console.log(pixels.length)
-      console.log(pixelsRaw.length)
-
+      
       // defining the sobel convolution matrices...
       var kx = [[-1, 0, 1],[-2, 0, 2],[-1, 0, 1]];
       var ky = [[-1, -2, -1],[0,  0,  0],[1,  2,  1]];
@@ -43,7 +40,9 @@ document.getElementById("file-select").addEventListener("change", function(e) {
         // ignore edge pixels since Sobel's requires a 3x3 block around pixel
         if (x == 0 || y == 0 || x == img.width-1 || y == img.height-1) {
           continue
-        }
+        } // (note: this will produce a matrix that is -2 width and -2 height)
+        x--
+        y--
 
         if (eMatrix.length - 1 < y) {
           eMatrix.push([])
@@ -54,7 +53,7 @@ document.getElementById("file-select").addEventListener("change", function(e) {
         // loop through 3x3 area around pixel
         for(a = 0; a < 3; a++) {
           for(b = 0; b < 3; b++) {            
-            var index = ((x + a - 1) + (y + b - 1) * img.width) * 4;
+            var index = ((x + a) + (y + b) * img.width) * 4;
 
             // Y = 0.299 * R + 0.587 * G + 0.114 * B
             var brightness =  0.299 * pixelsRaw[index] + 0.587 * pixelsRaw[index+1] + 0.114 * pixelsRaw[index+2]
@@ -64,14 +63,48 @@ document.getElementById("file-select").addEventListener("change", function(e) {
           }
         }
         var mag = Math.sqrt(magX*magX + magY*magY)
-        // pixels[i] = mag
-        // pixels[i+1] = mag
-        // pixels[i+2] = mag
-        // pixels[i+3] = 255
         eMatrix[y][x] = mag
-        
       }
-      console.log(eMatrix)
+
+      // dp table: element i is an array for the path from pixel i that has [0] = total energy cost for said path, [1 ... n] = the actual path
+      var paths = []
+      var startX = 0
+      for (y = 0; y < eMatrix.length; y++) {
+        paths[y] = [] // create x array for paths[y]
+        for (x = 0; x < eMatrix[y].length; x++) {
+          paths[y][x] = {} 
+
+          // dp base case
+          if (y == 0) {
+            paths[y][x].cost = eMatrix[y][x]
+            console.log(eMatrix[y][x])
+            paths[y][x].path = [[y, x]]
+          } else {
+            // compare to the nodes on top of current node to get min path
+            var lx = x
+            if (x > 0 && paths[y-1][x-1].cost < paths[y-1][lx].cost) {
+              lx = x-1
+            }
+            if (x < paths[0].length-1 && paths[y-1][x+1].cost < paths[y-1][lx].cost) {
+              lx = x+1
+            }
+            paths[y][x].cost = eMatrix[y][x] + paths[y-1][lx].cost
+            paths[y][x].path = paths[y-1][lx].path.concat([[y, x]])
+
+            // find complete path that has the lowest total energy cost
+            if (y == eMatrix.length-1 && paths[y][x].cost < paths[y][startX].cost) {
+              startX = x
+            }
+          }
+        }
+      }
+
+      rCtx.fillStyle = "red"
+      var seam = paths[paths.length-1][startX].path
+      for (i = 0; i < seam.length; i++) {
+        rCtx.fillRect(seam[i][1], seam[i][0], 1, 1)
+      }
+
       // rCtx.putImageData(renderImage, 0, 0)
     }
     img.src = event.target.result
